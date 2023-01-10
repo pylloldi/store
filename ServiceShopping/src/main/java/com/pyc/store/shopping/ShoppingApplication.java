31768b237e786a1fac85dc23dbedd6c0d9d6a965
pylloldi <pylloldi@gmail.com>
2023-01-10 01:33:06 -0600

    update product and shopping whit eureka

--- ServiceShopping/src/main/java/com/pyc/store/shopping/ShoppingApplication.java
+++ ServiceShopping/src/main/java/com/pyc/store/shopping/ShoppingApplication.java
@@ -2,8 +2,10 @@ package com.pyc.store.shopping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ShoppingApplication {
 
 	public static void main(String[] args) {
