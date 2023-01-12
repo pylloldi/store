4962cdc997601afd8afbf683c1cb7b1d4ea8cb2f
pylloldi <pylloldi@gmail.com>
2023-01-11 23:38:06 -0600

    update feing with hystrix

--- ServiceShopping/src/main/java/com/pyc/store/shopping/client/CustomerClient.java
+++ ServiceShopping/src/main/java/com/pyc/store/shopping/client/CustomerClient.java
@@ -6,10 +6,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "customer-service", fallback = CustomerHystrixFallbackFactory.class)
+@FeignClient(name = "customer-service", path = "/customers") //, fallback = CustomerHystrixFallbackFactory.class)
 public interface CustomerClient {    
     
-    @GetMapping(value = "/customers/{id}")
+    @GetMapping(value = "/{id}")
     public ResponseEntity<Customer> getCustomer(@PathVariable("id") long id);
     
 }
