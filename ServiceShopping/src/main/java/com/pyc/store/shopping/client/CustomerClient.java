eecb24cafc865b1125a41004c2d6f563234fa489
pylloldi <pylloldi@gmail.com>
2023-01-17 05:15:00 -0600

    circuit braker

--- ServiceShopping/src/main/java/com/pyc/store/shopping/client/CustomerClient.java
+++ ServiceShopping/src/main/java/com/pyc/store/shopping/client/CustomerClient.java
@@ -8,20 +8,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "customer-service", path = "/customers") //, fallback = CustomerHystrixFallbackFactory.class)
-public interface CustomerClient {    
-    
+public interface CustomerClient {
+
     @GetMapping(value = "/{id}")
-    @Bulkhead(name = "customer-service", fallbackMethod = "fallbackCustomer")
+    @Bulkhead(name = "getCustomerCircuitBreaker", fallbackMethod = "fallbackCustomer")
     public ResponseEntity<Customer> getCustomer(@PathVariable("id") long id);
-    
-    
+
     default ResponseEntity<Customer> fallbackCustomer() {
         Customer customer = Customer.builder()
                 .firstName("none")
                 .lastName("none")
                 .email("none")
                 .photoUrl("none").build();
-        
+
         return ResponseEntity.ok(customer);
     }
 }
